6d72d69b8efe57c5f3f8129e127c1e31b7eec379
0rientd <0rientd>
2020-06-21 00:29:39 -0300

    Added a call to a function tu update data.json

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import json
 import os
+from .main import main
 
 app = Flask(__name__)
 app.secret_key = b'1sad564as89123dkmk'
@@ -8,6 +9,7 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def index():
+    main()
     file = open("app/data.json",)
     json_data = json.load(file)
     return render_template('index.html', data = json_data)
