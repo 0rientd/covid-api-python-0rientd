4bf31a930f98387313dcf573d4d8ab84945d714a
0rientd <0rientd>
2020-06-21 00:38:06 -0300

    Fixed a problem with code

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import json
 import os
-from .main import main
+import main
 
 app = Flask(__name__)
 app.secret_key = b'1sad564as89123dkmk'
@@ -9,7 +9,7 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def index():
-    main()
+    main.main()
     file = open("app/data.json",)
     json_data = json.load(file)
     return render_template('index.html', data = json_data)
